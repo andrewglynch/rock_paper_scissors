d04794ff3fe1269800c080c0eeb84276e2f342c1
Andrew Lynch <andrewlynch88@gmail.com>
2019-08-12 20:10:28 +0100

    updated file

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -20,13 +20,36 @@ else print("Great! Let's play Rock Paper Scissors!")
 time.sleep(1)
 
 
-# Define different computer guesses
+# set up 'play again' variable that decides if game will be repeated
+play_again = 'Y'
 
+# initialise scores
+computer_score = 0
+player_score = 0
+
+# Define different computer guesses
 rps = ['Rock', 'Paper', 'Scissors']
 
-# Randomly generate an element from the list of possible computer guesses
-computer_guess = rps[randint(0,2)]
+# Set up continuous play loop
+while play_again == 'Y':
 
-# Get player input
+    # Randomly generate an element from the list of possible computer guesses
+    computer_guess = rps[randint(0,2)]
 
-player_guess = input()
+    # Get player input
+    print("What do you want to play? Rock, Paper or Scissors?")
+    print("Enter your turn: ")
+    player_guess = input()
+    time.sleep(2)
+    print("ooooo.......")
+    time.sleep(1)
+
+    # return result
+
+    if computer_guess == player_guess:
+        print("It's a tie! What are the odds?!")
+    elif computer_guess == 'Rock' and player_guess == 'Paper':
+        print("You win!")
+        print()
+
+    print("Play again? (Y/N)")
